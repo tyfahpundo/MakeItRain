d62062f49db2d9a8b0b022c7bba375752d55d8e9
tyfahpundo <tafadzwaallentyfah@gmail.com>
2021-11-13 14:12:28 +0200

    Added a switch case for different money values

--- app/src/main/java/com/afrosoft/makeitrain/MainActivity.java
+++ app/src/main/java/com/afrosoft/makeitrain/MainActivity.java
@@ -2,7 +2,10 @@ package com.afrosoft.makeitrain;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.icu.text.NumberFormat;
 import android.os.Build;
 import android.os.Bundle;
@@ -29,5 +32,20 @@ public class MainActivity extends AppCompatActivity {
         moneyCounter += 1000;
         moneyValue.setText(String.valueOf(numberFormat.format(moneyCounter)));
         Log.d("MIR", "onClick: "+ moneyCounter);
+        switch (moneyCounter){
+            case 20000:
+                moneyValue.setTextColor(ContextCompat.getColor(MainActivity.this,
+                        R.color.green));
+                break;
+            case 40000:
+                moneyValue.setTextColor(ContextCompat.getColor(MainActivity.this,
+                        R.color.purple_200));
+                break;
+            case 80000:
+                moneyValue.setTextColor(Color.GRAY);
+                break;
+            default:
+                moneyValue.setTextColor(Color.WHITE);
+        }
     }
 }
